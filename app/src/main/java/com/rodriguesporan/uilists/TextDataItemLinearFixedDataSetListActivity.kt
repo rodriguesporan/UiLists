6fceb28ceaf28be5f79c66ab657ac414a0818fcb
rodriguesporan <rodriguesporan@gmail.com>
2023-03-17 11:16:16 -0300

    init project

---
+++ app/src/main/java/com/rodriguesporan/uilists/TextDataItemLinearFixedDataSetListActivity.kt
@@ -0,0 +1,11 @@
+package com.rodriguesporan.uilists
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextDataItemLinearFixedDataSetListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_data_item_linear_fixed_data_set_list)
+    }
+}
